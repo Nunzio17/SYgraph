ec1711512d6909c8e9790ea80527de91d8552c61
Nunzio17 <n.avino2@studenti.unisa.it>
2026-04-29 12:36:25 +0200

    Added include for kcore.hpp in sygraph.hpp

--- include/sygraph/sygraph.hpp
+++ include/sygraph/sygraph.hpp
@@ -38,6 +38,7 @@
 #include <sygraph/algorithms/cc.hpp>
 #include <sygraph/algorithms/sssp.hpp>
 #include <sygraph/algorithms/tc.hpp>
+#include <sygraph/algorithms/kcore.hpp>
 
 // Include sync
 #include <sygraph/sync/atomics.hpp>
